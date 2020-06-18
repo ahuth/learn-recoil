14553472a97e8ab42bd263aba269371e2c828111
Andrew Huth <andrew@huth.me>
2020-06-18 07:26:57 -0400

    Add filters

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useRecoilValue } from 'recoil';
 import TodoItem from './TodoItem';
 import TodoItemCreator from './TodoItemCreator';
+import TodoListFilters from './TodoListFilters';
 import { filteredTodoListState } from '../selectors';
 
 export default function TodoList() {
@@ -10,6 +11,7 @@ export default function TodoList() {
   return (
     <div>
       <TodoItemCreator />
+      <TodoListFilters />
       {todoList.map((todoItem) => (
         <TodoItem key={todoItem.id} item={todoItem} />
       ))}
